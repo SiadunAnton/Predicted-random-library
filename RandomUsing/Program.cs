73db61ce5ade459a31ddade59227ba43a526859e
Anton Siadun <antonsiadun@yandex.by>
2024-03-17 21:45:13 +0300

    Upload project files

---
+++ RandomUsing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PredictedRandom;
+
+namespace RandomUsing
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var arrayFactory = new ArrayFactory<int>(9, (1,33), (2,33), (5,33)); 
+
+            
+            for (int i = 0; i < 5; i++)
+            {
+                var anArray = arrayFactory.Generate();
+
+                foreach (var item in anArray)
+                    Console.Write($"{item}  ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
